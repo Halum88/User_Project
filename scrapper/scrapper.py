38ee2e6eb65ef884c5c3ffd7734ff8dc259857b7
Halum88 <1234as4321@mail.ru>
2023-02-20 16:42:19 +0300

    del teg from ip

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,7 +1,7 @@
 import os
 from bs4 import BeautifulSoup
 from requests import get
-
+import re
 
 
 base_url = 'https://free-proxy-list.net/'
@@ -9,30 +9,29 @@ count = 0
 dict = []
 #def init_db():
 
+
 def scrapping():
     response = get(base_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     proxy_list = soup.find('div', class_ = 'table-responsive fpl-list')
     global count
     global dict
-    
-    
-    
+
     try:
         for all_proxy in proxy_list:
-            ip = all_proxy.find_all('tr')
-            for j in ip:
-                ip_2 = j.find('td')
-                dict.append(ip_2)
-        print(dict)
+            tr = all_proxy.find_all('tr')
+            for j in tr:
+                td = j.find('td')
+                ip = re.sub('<[^>]+>',"",str(td))
+                dict.append(ip)              
     
-
-  #all_task.find('div', {'class':'divider'}).find('span', {'class':'comments-count'})['title']          
-            
-    
-        
     except Exception as error:
         return error
+
+
+
+
+
 
 #___main___#
 
