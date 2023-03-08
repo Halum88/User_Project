fd46f8e7fa073e516bb5eeedf163bd14dd5f9473
Halum88 <1234as4321@mail.ru>
2023-03-08 11:55:51 +0300

    add check proxies

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -47,7 +47,7 @@ def maxim_id():
             max_id = 0 
             
     except Exception as error:
-        raise Exception('Nо connect to DB!', error) 
+        raise Exception('Nо max(id) to DB!', error) 
     finally:
         db.close()
 
@@ -56,24 +56,8 @@ def scrap_proxy():
     response = get(base_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     proxy_list = soup.find('table', class_ = 'table table-striped table-bordered').find_all('tr')[1:]
-    global max_id
     global proxy_dict
     
-    
-    ###Подключаемся к БД###
-    try:
-        db = psycopg2.connect(
-            database = db_name, 
-            user = user_name, 
-            password = user_pw, 
-            host="127.0.0.1", 
-            port="5432"
-            )
-        cursor = db.cursor()
-        
-    except Exception as error:
-        raise Exception('Not connect DB', error)
-        
     
     ###Получаем список хостов###
     for host_list in proxy_list:
@@ -83,44 +67,45 @@ def scrap_proxy():
         host = f"{ip}:{port}"
         proxy_dict.append(host)
         
-        
-    ###Запись хостов в БД###
-        try:
-            max_id += 1
-            cursor.execute('''INSERT INTO proxy VALUES (%s,%s) 
-                              ON CONFLICT (host) DO UPDATE 
-                              SET host=%s
-                              ''',[max_id, host, host])
-            
-        except Exception as error:
-            raise Exception('Recording error', error)
-
-            
-    db.commit()
-    db.close()
     time.sleep(1 + (random.random() * (9 - 5)))
-
-    # proxy_random = random.choice(proxy_dict)    # рандом прокси
 
 
 def check_proxy():
+    try:
+        db = psycopg2.connect(
+            database = db_name, 
+            user = user_name, 
+            password = user_pw, 
+            host="127.0.0.1", 
+            port="5432"
+            )
+        cursor = db.cursor()     
+    except Exception as error:
+        raise Exception('Not connect DB', error)
+    
     for px in proxy_dict:
         proxies = {"http": px, "https": px}
         try:
-            prox = get('http://icanhazip.com', proxies=proxies, timeout=1.3).text.strip()
-            print(prox)
+            get('http://icanhazip.com', proxies=proxies, timeout=1.3).text.strip()
+            try:
+                max_id += 1
+                cursor.execute('''INSERT INTO proxy VALUES (%s,%s) 
+                                ON CONFLICT (host) DO UPDATE 
+                                SET host=%s
+                                ''',[max_id, px, px])
+            except Exception as error:
+                raise Exception('Recording error', error)
         except Exception as e:
-            proxy_dict.remove(px)
-    print(len(proxy_dict))
+           proxy_dict.remove(px)
+    
+    db.commit()
+    db.close()
+
   
-            
-
-
     
 ###___main___###
 init_db()
 maxim_id()
 scrap_proxy()
-print(len(proxy_dict))
 check_proxy()
 
