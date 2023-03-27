f4b341c2f13c3ff1e4898386dd4452320edea9cd
Halum88 <1234as4321@mail.ru>
2023-03-27 16:29:41 +0300

    Парсит на нужном сайте без проблем

--- prx_scrapper/proxy_scrap.py
+++ prx_scrapper/proxy_scrap.py
@@ -5,12 +5,15 @@ import psycopg2
 import time
 from requests import get
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 
 base_url = 'https://free-proxy-list.net/'
 db_name = os.environ['DB_NAME']
 user_name = os.environ['USER_NAME']
 user_pw = os.environ['USER_PW']
+ua = UserAgent()
+headers = {'User-Agent': ua.random} #рандомный user-agent
 
 
 count = 0
@@ -18,8 +21,7 @@ count_1 = 0
 count_2 = 0
 max_id = 1
 proxy_dict = []
-prx_https = []
-prx_http = []
+prx_list = []
 
 ###Инициализая БД###
 def init_db():
@@ -67,42 +69,29 @@ def scrap_proxy():
                     td = tr.find_all('td')
                     ip = td[0].text.strip()
                     port = td[1].text.strip()
-                    protocol = td[6].text.strip().lower()
-                    if protocol == 'yes':
-                        prx_https.append(f"{ip}:{port}")
-                    elif protocol == 'no':
-                        prx_http.append(f"{ip}:{port}")            
+                    prx_list.append(f"{ip}:{port}")
+         
     except Exception as e:
         raise Exception("Error:", response.status_code)
 
 
 def check():
     global count_1
-    global count_2
     global proxy_dict
     try:
-        for i in prx_https:
-            response = requests.get("https://www.google.com", proxies={'https://':i}, timeout=5)
+        for i in prx_list:
+            response = requests.get("https://www.1cont.ru/contragent/by-region", headers=headers, proxies={'https://':i,'http://':i}, timeout=5)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            test = soup.find('div', class_='content__inner lr-ident').find('h1', class_='h2').text
+            print(test, i)
             if response.status_code == 200:
                 count_1 += 1  
                 proxy_dict.append(i) 
             else:
-                print('https: NOT OK')
-        print('https получено:', count_1)
+                print('connect: NOT OK')
+        print('proxies получено:', count_1)
     except Exception as e:
-        print('ERROR HTTPS:', e)
-
-    try:
-        for i in prx_http:
-            response = requests.get("https://www.google.com", proxies={'http://':i}, timeout=5)
-            if response.status_code == 200:
-                count_2 += 1
-                proxy_dict.append(i) 
-            else:
-                print('http: NOT OK')
-        print('http получено:', count_2)
-    except Exception as e:
-        print('ERROR HTTP:', e)
+        print('ERROR connect:', e)
 
 
 def rec_db():
@@ -132,4 +121,4 @@ init_db()
 maxim_id()
 scrap_proxy()
 check()
-rec_db()
+# rec_db()
