205b8c091a7e0c3208df92fee730a952feb41d27
Halum88 <114135252+Halum88@users.noreply.github.com>
2023-10-03 22:03:54 +0300

    Feature/fake users (#3)
    * def рандомного прокси из бд
    
    * Тест подмены ip ок
    
    * initial commit
    
    * Пытаюсь подключиться к сайту для парсинга
    
    * err с конектотм к сайту рабоотает 1 из 10 хостов
    
    * убрал лишнее. Не подключается к сайту
    
    * К сайту нет подключения
    
    * Парсинг сайт без подмены ip
    
    * Добавил запись в бд хороших прокси
    
    * СОхранение и выход из бд
    
    * initial commit
    
    * Сделать запись рабочих прокси
    
    * Починил запись работующий прокси в бд
    
    * Решил проблему с ошиибкой прокси
    
    * убрал комменты
    
    * ddl region table
    
    * Переименовал БД добавил таблицы
    
    * Добавил запись регионов в бд
    
    * Проблема с дублем записи регионов
    
    * добавил скрап данных с сайта
    
    * Сделал запись в БД
    
    * add region.sql and rec in DB
    
    * Добавил парсинг юзеров по регионам
    
    * Парсинг ИП с записью в БД по регионам
    
    * add ООО в запись. add таблицу.  ошибка rec ooo
    
    * Изменил структуру rand_proxi

--- env.bat
+++ env.bat
@@ -1,4 +1,5 @@
 setx APP_HOME "C:\Program Files (x86)\Programming\Projects\User_Project"
 setx USER_NAME "auhor"
 setx USER_PW "qwerty"
-setx DB_NAME "proxy_db"
+setx DB_NAME "user_project"
+setx DB_PROXY "proxy_db"
